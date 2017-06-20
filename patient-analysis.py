9ce2a9796bb78714ccb34008529b944bc7f524ca
Nargess Memarsadeghi <Nargess.Memarsadeghi@nasa.gov>
2017-06-20 11:08:56 -0400

    added some more comments and code

--- patient-analysis.py
+++ patient-analysis.py
@@ -1,10 +1,14 @@
 import numpy
 import matplotlib.pyplot as plt
 
-data = loadtxt(fname='data/inflammation1.csv', delimiter=',')
+data = np.loadtxt(fname='data/inflammation1.csv', delimiter=',')
 
 print(data)
 
+#finding dimension of data
+print(data.shape)
+
 image-1=plt.plot(data)
 
+#plotting data
 plt.show(image-1)
